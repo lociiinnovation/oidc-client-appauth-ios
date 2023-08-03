df7a55f7265152bf816b977a8baed9ba7a34fda7
som1361 <s.zahedian@gmail.com>
2023-08-03 10:27:12 +1000

    Initial Commit

---
+++ oidc-client-appauth-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  oidc-client-appauth-ios
+//
+//  Created by Locii on 3/8/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ oidc-client-appauth-ios/oidc_client_appauth_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  oidc_client_appauth_iosApp.swift
+//  oidc-client-appauth-ios
+//
+//  Created by Locii on 3/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct oidc_client_appauth_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
